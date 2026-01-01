7c111bfc3c007dd86918ce9464dcf217acd97954
Max Khabibullin <max.habibullin@gmail.com>
2026-01-01 21:52:01 +0300

    fix port section
    опечатка

--- install.sh
+++ install.sh
@@ -99,12 +99,8 @@ services:
       POSTGRES_USER: n8n
       POSTGRES_PASSWORD: ${PG_PASSWORD}
       POSTGRES_DB: n8n
-    ports:
-     - "127.0.0.1:5432:5432"
     volumes:
       - /docker_volumes/postgres_n8n:/var/lib/postgresql/data
-    # Опционально: если нужен доступ через SSH-туннель
-    # (БД будет доступна только на самом VPS по 127.0.0.1)
     ports:
       - "127.0.0.1:5432:5432"
     networks:
